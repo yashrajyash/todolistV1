e22f5d97951ff06194df5f8386e8ca585d4871e4
yashrajyash <yashrajyash18@gmail.com>
2021-06-26 18:12:49 +0530

    files initialized

---
+++ date.js
@@ -0,0 +1,32 @@
+// can use exports and module.exports
+//both are same
+
+module.exports.getDate = function () {
+    const today = new Date();
+
+    const options = {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long',
+    };
+
+    const day = today.toLocaleDateString('en-US', options);
+
+    return day;
+}
+
+
+exports.getDay = function () {
+    const today = new Date();
+
+    const options = {
+        weekday: 'long',
+    };
+
+    const day = today.toLocaleDateString('en-US', options);
+
+    return day;
+}
+
+
+// console.log(module.exports);
